f9a359f0cacd54856f82428d228c210a7092ba1b
tommo593 <tomdevwork@gmail.com>
2024-03-20 13:09:44 +0000

    Loads of stuff

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,69 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
 import { FaPhone, FaEnvelope, FaFile } from "react-icons/fa";
 
+type Contact = {
+  parameter: boolean;
+};
+
 const Contact = () => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [stateMessage, setStateMessage] = useState(null);
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setStateMessage("Message sent!");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage("Something went wrong, please try again later");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        }
+      );
+
+    // Clears the form after sending the email
+    e.target.reset();
+  };
   return (
     <div>
       <h1 className="text-center py-8">Contact</h1>
+      <div>
+        {""}
+        <form
+          onSubmit={sendEmail}
+          className="flex flex-col max-w-80 justify-center"
+        >
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input
+            type="submit"
+            value="Send"
+            disabled={isSubmitting}
+            className="rounded border border-gray-100 shadow p-2 transition bg-gradient-to-r from-blue-200 to-blue-400 cursor-pointer max-w-28"
+          />
+          {stateMessage && <p>{stateMessage}</p>}
+        </form>
+      </div>
       <div className="flex flex-row justify-center space-x-8 pb-8">
         <div>
           <FaPhone />
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,26 +1,16 @@
-import { useState } from "react";
-
 const Projects = () => {
-  const [open, setOpen] = useState<boolean>(false);
-
   return (
     <div className="py-8">
-      <h1 className="text-center py-4">Projects</h1>
-      <div className="text-center">
-        <button
-          className="animate-pulse font-bold py-4"
-          onClick={() => setOpen(!open)}
-        >
-          Click
-        </button>
-        {open && <p className="pb-4">You rascal</p>}
-      </div>
+      <h1 className="text-center py-12">Projects</h1>
       <div className="flex flex-row justify-center space-x-8 pt-4 cursor-pointer">
         <a href="https://github.com/tommo593/portfolio-vite">
           <img
             src="screenshot3.png"
-            className="max-h-96 rounded rounded-r-none transition hover:scale-125 duration-300"
+            className="max-h-96 rounded-md rounded-r-none transition hover:scale-125 duration-300"
           />
+          {/*<label className="absolute z-10 bg-white bg-opacity-75">
+            This page's code!
+  </label>*/}
         </a>
         <a href="https://job-satisfaction-ts.vercel.app/">
           <img
@@ -31,7 +21,7 @@ const Projects = () => {
         <a href="https://github.com/tommo593/hagl-nextjs">
           <img
             src="screenshot1.png"
-            className="max-h-96 rounded rounded-l-none transition hover:scale-125 duration-300"
+            className="max-h-96 rounded-md rounded-l-none transition hover:scale-125 duration-300"
           />
         </a>
       </div>
