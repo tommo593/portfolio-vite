91a30974426da9d9b2d79b54f81ad6b9049e2f62
tommo593 <tomdevwork@gmail.com>
2024-03-28 04:00:32 +0000

    Stuff

--- src/components/EmblaCarousel.tsx
+++ src/components/EmblaCarousel.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect } from 'react'
 import { EmblaCarouselType } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
 
 export function EmblaCarousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel()
+  const [emblaRef, emblaApi] = useEmblaCarousel(emblaRoot, { loop: false }, [WheelGesturesPlugin()])
 
   const logSlidesInView = useCallback((emblaApi: EmblaCarouselType) => {
     console.log(emblaApi.slidesInView())
@@ -14,7 +15,7 @@ export function EmblaCarousel() {
   }, [emblaApi, logSlidesInView])
 
   return (
-    <div className="embla border border-red-500" ref={emblaRef}>
+    <div className="embla border w-1/4 m-auto pt-12 pb-12" ref={emblaRef}>
       <div className="embla__container">
         <div className="embla__slide">Slide 1</div>
         <div className="embla__slide">Slide 2</div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,24 +4,21 @@ const Projects = () => {
     <div className="py-8">
       <h3 className="text-center py-12">Projects</h3>
       <div className="flex flex-row justify-center space-x-8 pt-4 cursor-pointer">
-        <a href="https://github.com/tommo593/portfolio-vite">
+        <div><a href="https://github.com/tommo593/portfolio-vite">
           <img
             src="screenshot3.png"
-            className="max-h-96 rounded-md rounded-r-none transition hover:scale-125 duration-300"
-          />
-        </a>
-        <a href="https://job-satisfaction-ts.vercel.app/">
+            className="project-image max-h-96 rounded-md rounded-r-none transition hover:scale-125 duration-300"
+          /></a></div> 
+       <div><a href="https://job-satisfaction-ts.vercel.app/">
           <img
             src="screenshot2.png"
-            className="max-h-96 transition hover:scale-125 duration-300"
-          />
-        </a>
-        <a href="https://github.com/tommo593/hagl-nextjs">
+            className="project-image max-h-96 transition hover:scale-125 duration-300"
+          /></a></div> 
+       <div><a href="https://github.com/tommo593/hagl-nextjs">
           <img
             src="screenshot1.png"
-            className="max-h-96 rounded-md rounded-l-none transition hover:scale-125 duration-300"
-          />
-        </a>
+            className="project-image max-h-96 rounded-md rounded-l-none transition hover:scale-125 duration-300"
+          /></a></div> 
       </div>
     </div>
   );
