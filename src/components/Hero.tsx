71b048d03e880fa630dadd92a93febbff26a556b
tommo593 <tomdevwork@gmail.com>
2024-04-05 16:24:30 +0100

    logo change, please fit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero: React.FC = () => {
   return (
     <div>
-      <img src="coding_bruv_full.png" alt="Coding Bruv" title="Coding Bruv" className="m-auto max-w-sm pb-8 pt-8" />
+      <img src="coding_bruv_full.png" alt="Coding Bruv" title="Coding Bruv" className="m-auto max-w-80 pb-8 pt-8" />
     </div>
   );
 };
