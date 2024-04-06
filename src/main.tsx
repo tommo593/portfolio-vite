b7528ea1400820854c352ad405e52c44236f1283
tommo593 <tomdevwork@gmail.com>
2024-04-06 09:26:10 +0100

    Added Router-ish

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+/*
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutMe from '../src/pages/AboutMe.tsx';
+import ContactMe from '../src/pages/ContactMe.tsx';
+import MyWork from '../src/pages/MyWork.tsx';
+*/
+import App from './App.tsx';
+import './index.css';
+
+/* const router = createBrowserRouter([
+  {
+    path: 'AboutMe',
+    element: <AboutMe />,
+  },
+  {
+    path: 'ContactMe',
+    element: <ContactMe />,
+  },
+  {
+    path: 'MyWork',
+    element: <MyWork />,
+  },
+]); */
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default AboutMe;
