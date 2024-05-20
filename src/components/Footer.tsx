fc7c422011cfd2c9f4144b29f85f9dd010986a45
tommo593 <tomdevwork@gmail.com>
2024-05-20 22:43:42 +0100

    added padding to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <p className="footer-text m-auto pb-12 text-center">
+      <p className="footer-text m-auto py-2 pb-12 text-center">
         Thank you for taking the time to look at my stuff...
         <br />
         <br />
