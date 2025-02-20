23071fc07a633bb2565f2165a4d46dcddfcaa93f
tommo593 <tomdevwork@gmail.com>
2025-02-20 02:02:50 +0000

    Updated contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -30,16 +30,30 @@ export const Contact = () => {
       </div>
       <div>
         <form ref={form} onSubmit={sendEmail} className="m-auto flex max-w-96 flex-col justify-between rounded">
-          <label className="pb-2">Name</label>
-          <input type="text" name="user_name" className="border-border_gray mb-4 h-8 rounded border" required />
-          <label className="pb-2">Email</label>
-          <input type="email" name="user_email" className="border-border_gray mb-4 h-8 rounded border" required />
-          <label className="pb-2">Message</label>
-          <textarea name="message" className="border-border_gray mb-4 h-32 resize-none rounded border pb-8" required />
+          <input
+            type="text"
+            name="user_name"
+            className="border-border_gray mb-4 h-8 rounded border p-1"
+            placeholder="name"
+            required
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="border-border_gray mb-4 h-8 rounded border p-1"
+            placeholder="email"
+            required
+          />
+          <textarea
+            name="message"
+            className="mb-4 h-32 resize-none rounded border p-1 pb-8"
+            placeholder="message"
+            required
+          />
           <input
             type="submit"
             value="Send"
-            className="btn text-off-black bg-mustard-yellow items-center rounded border border-transparent py-2 text-center shadow"
+            className="cursor-pointer rounded border border-transparent bg-mustard-yellow py-2 text-center text-off-black hover:opacity-80 active:bg-light-grey"
           />
         </form>
       </div>
